342e4cb74d987da3731ddcafd701ba21656805d6
mrdcvlsc <mrdcvlsc@gmail.com>
2025-09-09 15:00:03 +0800

    copy main.cpp to test source code

--- test/source/speech-vs-non-speech_test.cpp
+++ test/source/speech-vs-non-speech_test.cpp
@@ -1,8 +1,35 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include <ATen/Parallel.h>
+#include <SFML/Audio.hpp>
+#include <torch/script.h>
+
 #include "lib.hpp"
 
-auto main() -> int
+auto main(int argc, char** argv) -> int
 {
-  auto const lib = library {};
+  std::vector<std::string> files = {"audio-samples/speech.ogg",
+                                    "audio-samples/noise.ogg"};
+  if (argc > 1) {
+    files.clear();
+    for (int i = 1; i < argc; ++i) {
+      files.emplace_back(argv[i]);
+    }
+  }
 
-  return lib.name == "speech-vs-non-speech" ? 0 : 1;
+  std::string mode_path = "silero_vad_v6.jit";
+  silero_vad model(mode_path);
+
+  for (const auto& audio_file : files) {
+    std::cout << "\n\nfinal result : " << model.has_speech(audio_file) << '\n';
+  }
+
+  return 0;
 }
